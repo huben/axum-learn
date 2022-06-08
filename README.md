238f6d19a8b9d9243c59d1ce2d028746397ec6d7
huben <ben.hu@haochezhu.club>
2022-06-08 14:59:41 +0800

    feat: router

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 
+https://www.yuque.com/u2090556/zc5mnz/zym98v
+
+
 ```
 cargo run --bin hello
 
--- router/src/router.rs
+++ router/src/router.rs
@@ -1,12 +1,58 @@
 use axum::{
-  routing::get,
+  extract::Path,
+  routing::{ get, any },
   Router,
+  response::IntoResponse,
+  http::{
+    StatusCode,
+    Uri,
+  }
 };
+
+async fn captures(Path((key, id)): Path<(String, u64)>) -> String {
+  format!("capture path key = {}, id = {}", key, id)
+}
+
+async fn serve_asset(Path(path): Path<String>) -> String {
+  format!("wildcards path = {}", path)
+}
+
+pub fn v1() -> Router {
+  let router = Router::new()
+    .route("/test", get(|| async { "hello v1" }))
+
+    ;
+  router
+}
+
+pub fn v2() -> Router {
+  let router = Router::new()
+    .route("/test", get(|| async { "hello v2" }))
+
+    ;
+  router
+}
+
+async fn fallback(uri: Uri) -> impl IntoResponse {
+  (StatusCode::NOT_FOUND, format!("File no found {}", uri))
+}
 
 pub fn new() -> Router {
   let router = Router::new()
-    .route("/", get(|| async { "hello router" }));
+    .route("/", get(|| async { "hello router" }))
+    .route("/foo", get(|| async { "hello foo" }))
 
+    .route("/captures/:key/:id", get(captures))
+
+    .route("/assets/*path", get(serve_asset))
+
+    .nest("/v1", v1())
+    .nest("/v2", v2())
+
+    .merge(v1())
+    
+    .fallback(any(fallback))
+    ;
     
   router
 }
